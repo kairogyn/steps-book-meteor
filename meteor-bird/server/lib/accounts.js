93f285f3aefceda94da57695a69c4c1b9818029b
kairogyn <kairogyn@gmail.com>
2014-10-14 17:41:14 -0300

    Feature alterção dos dados do usuario, atualização do perfil e obtenção dos dados apos login via facebook

--- meteor-bird/server/lib/accounts.js
+++ meteor-bird/server/lib/accounts.js
@@ -1,4 +1,12 @@
 Accounts.onCreateUser(function(options, user){
-  user['profile'] = options.profile;
+  if(user.services.facebook){
+    var facebook = user.services.facebook;
+    user['profile'] = {
+      name: facebook.name
+   };
+  }
+  else{ 
+    user['profile'] = options.profile;
+  }
   return user;
 });
