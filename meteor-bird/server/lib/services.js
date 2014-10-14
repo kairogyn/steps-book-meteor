54cdf947fdf480eed6f0acc25182b39696f0f6f6
kairogyn <kairogyn@gmail.com>
2014-10-14 16:45:39 -0300

    Feature login com facebook ok

---
+++ meteor-bird/server/lib/services.js
@@ -0,0 +1,7 @@
+ServiceConfiguration.configurations.remove({});
+
+ServiceConfiguration.configurations.insert({
+  service: 'facebook',
+  appId: '1528237377419506',
+  secret: 'd649bf474035b48cc4f32ae71ad7f864'
+});
